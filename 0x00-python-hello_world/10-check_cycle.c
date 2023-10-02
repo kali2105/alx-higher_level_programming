c49d7aab5164ab299224671dfab39b353bf5e232
kali2105 <anassalaoui0593@gmail.com>
2023-10-02 13:02:53 -0700

    fixing...

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,14 +8,12 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *slow = list;
+	listint_t *fast = list;
 	if (!list)
 	{
 	return (0);
 	}
-
-	listint_t *slow = list;
-	listint_t *fast = list;
-
 	while (fast && fast->next)
 	{
 		slow = slow->next;
