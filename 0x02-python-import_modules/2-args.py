ea49ea2a13d3da94e6dd1d1c360754a400d4d3e5
kali2105 <anassalaoui0593@gmail.com>
2023-10-05 12:11:46 -0700

    fixing...

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,15 @@
 
 if __name__ == "__main__":
     """program that print num of agrs in str"""
-    
+
     import sys
     arguments = sys.argv[1:]
 
-    num_arguments = len(arguments)
+    num_a = len(arguments)
 
-    if num_arguments == 0:
+    if num_a == 0:
         print("0 arguments.")
     else:
-        print("{} argument{}:".format(num_arguments, 's' if num_arguments > 1 else ''))
+        print("{} argument{}:".format(num_a, 's' if num_a > 1 else ''))
         for i, arg in enumerate(arguments, start=1):
             print("{}: {}".format(i, arg))
-
