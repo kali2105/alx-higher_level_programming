528c2a2fd25e6e2658b0894df56f6e82c806b3b2
kali2105 <anassalaoui0593@gmail.com>
2023-10-05 18:06:44 -0700

    fixing...

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """prg that matches bytecode given by Holberton School"""
+    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
     if a < b:
