4993079a7f280d162b17fc7d257be7aad3e487f4
kali2105 <anassalaoui0593@gmail.com>
2023-10-24 12:59:27 -0700

    updating...

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 ''' defining a square class'''
 
+
 class Square:
 ''' size and empty class'''
     def __init__(self, size):
+        ''' initialize the size attribute'''
         self.__size = size
